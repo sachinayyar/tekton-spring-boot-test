8a7f1b9a374d9d45bbccb7af449dbe2f7398d77f
Sachin Ayyar <91127350+sachinayyar@users.noreply.github.com>
2023-12-22 02:01:32 +0530

    Add files via upload

---
+++ src/main/java/com/redhat/developers/tektongitjibkn/MyController.java
@@ -0,0 +1,31 @@
+package com.redhat.developers.tektongitjibkn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @Autowired
+    private Environment environment;
+
+    final String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
+
+    String greeting;
+
+    private int count = 0; // simple counter to see lifecycle
+
+    @GetMapping("/namaste")    
+    public String index() {
+        greeting = environment.getProperty("GREETING","Namaste");
+        greeting = greeting + " From sachin" + count + " on " + hostname + "\n";
+        System.out.println(greeting);
+        return greeting;
+    }
+
+    @GetMapping("/hello")    
+    public String hello() {
+        return "hello";
+    }    
+}
